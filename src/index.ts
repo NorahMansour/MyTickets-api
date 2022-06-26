9d386deac63fccd0a4d99cc7a3e2c63f8c293188
NorahMansour <norahalsheaifi@gmail.com>
2022-06-26 14:21:56 +0300

    done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { server } from './server';
 
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
 
-/*const mongoose = any {}
-mongoose.co (()=>)*/
-
-
-server.listen({ port: 3000 }).catch((err) => {
-	server.log.error(err);
-	process.exit(1);
-});
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	})
